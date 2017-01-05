a64cb6124f531088b00faaf449ba14a90202f92c
Mehturt <mehturt@gmail.com>
2017-01-05 16:10:44 +0100

    Small updates using references

--- tplink.pl
+++ tplink.pl
@@ -54,6 +54,7 @@ sub path_with_token {
 
 # 1 - path
 # 2 - include_referer = true
+# return - HTTP::Response
 sub get_page {
 	my ($path, $referer) = @_;
 
@@ -75,9 +76,8 @@ sub get_page {
 	Dbg("Sending req for page $path with referer $referer");
 
 	my $response = $ua->request($request);
-	my $content = $response->content();
-	Dbg("Response: $content");
-	return $content;
+	Dbg("Response: " . $response->content());
+	return $response;
 }
 
 sub get_url_token {
@@ -86,46 +86,41 @@ sub get_url_token {
 	}
 
 	get_page('/', 0);
-	my $resp = get_page('/userRpm/LoginRpm.htm?Save=Save', 0);
-	if ($resp =~ /http:\/\/[^\/]+\/([A-Za-z0-9]+)/) {
+	my $response = get_page('/userRpm/LoginRpm.htm?Save=Save', 0);
+	if ($response->content() =~ /http:\/\/[^\/]+\/([A-Za-z0-9]+)/) {
 		$url_token = $1;
 		Dbg("URL token is: $url_token");
 	}
 }
 
 sub get_status_page {
-	get_page(path_with_token("/userRpm/StatusRpm.htm"));
+	return get_page(path_with_token("/userRpm/StatusRpm.htm"));
 }
 
 sub get_wireless_stats_page {
-	get_page(path_with_token("/userRpm/WlanStationRpm.htm"));
+	return get_page(path_with_token("/userRpm/WlanStationRpm.htm"));
 }
 
 sub get_wireless_stats_page_5g {
-	get_page(path_with_token("/userRpm/WlanStationRpm_5g.htm"));
+	return get_page(path_with_token("/userRpm/WlanStationRpm_5g.htm"));
 }
 
-# 1 - page
-# 2 - name
+# 1 - HTTP::Response
+# 2 - array name
+# 3 - returned array ref
 sub extract_js_array {
-	my ($page, $name) = @_;
-	my @clients = ();
-	if ($page =~ /$name\s+=\s+new\s+Array\(\n(.+?)\)/s) {
+	my ($page, $name, $aref) = @_;
+
+	if ($page->content() =~ /$name\s+=\s+new\s+Array\(\n(.+?)\)/s) {
 		my @array = split(/\n/, $1);
 		foreach (@array) {
 			Dbg("Array line: $_");
 			my @line = split(/,/);
-			push(@clients, $line[0]);
+			push(@$aref, $line[0]);
 		}
 	}
-	return @clients;
 }
 
-sub test {
-	my $content = "a\nb\nc\nvar hostList = new Array(\n\"18-1E-B0-66-2A-CB\", 5, 450256, 378696,\n\"10-92-66-7F-C6-62\", 5, 491146, 470950,\n0,0 );\nx\nd\n";
-	my @wlan_clients = extract_js_array($content, "hostList");
-	Dbg("Clients: " . scalar(@wlan_clients));
-}
 
 if ($ARGV[0] and $ARGV[0] eq "config" ) {
 	print << 'EOF';
@@ -164,39 +159,29 @@ $auth_cookie = build_auth_cookie($user, $pass);
 print "multigraph wireless_channel\n";
 
 my $status_page = get_status_page();
-my @status = extract_js_array($status_page, 'wlanPara');
-my $channel;
-if ($status[2] == 15) {
-	Dbg("Channel: $status[9]");
-	$channel = $status[9];
-}
-else {
-	Dbg("Channel: $status[2]");
-	$channel = $status[2];
-}
+my @status;
+extract_js_array($status_page, 'wlanPara', \@status);
+my $channel = ($status[2] == 15) ? $status[9] : $status[2];
 print "channel.value " . $channel . "\n";
 
-my @status_5g = extract_js_array($status_page, 'wlan5GPara');
-if ($status_5g[2] == 15) {
-	Dbg("Channel: $status_5g[9]");
-	$channel = $status_5g[9];
-}
-else {
-	Dbg("Channel: $status_5g[2]");
-	$channel = $status_5g[2];
-}
+my @status_5g;
+extract_js_array($status_page, 'wlan5GPara', \@status_5g);
+$channel = ($status_5g[2] == 15) ? $status_5g[9] : $status_5g[2];
 print "channel5.value " . $channel . "\n";
 
 print "multigraph connected_clients\n";
 
 my $wireless_stats_page = get_wireless_stats_page();
-my @wireless_clients = extract_js_array($wireless_stats_page, 'hostList');
+my @wireless_clients;
+extract_js_array($wireless_stats_page, 'hostList', \@wireless_clients);
 print "clients.value " . (scalar(@wireless_clients) - 1) . "\n";
 
 my $wireless_stats_page_5g = get_wireless_stats_page_5g();
-my @wireless_clients_5g = extract_js_array($wireless_stats_page_5g, 'hostList');
+my @wireless_clients_5g;
+extract_js_array($wireless_stats_page_5g, 'hostList', \@wireless_clients_5g);
 print "clients5.value " . (scalar(@wireless_clients_5g) - 1) . "\n";
 
 print "multigraph uptime\n";
-@status = extract_js_array($status_page, 'statusPara');
+@status = ();
+extract_js_array($status_page, 'statusPara', \@status);
 print "uptime.value " . $status[5] / 86400.0 . "\n";
